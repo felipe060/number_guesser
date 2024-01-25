23c773768146f5c0a75d267b1c0a0635d5a26f24
felipe060 <felipeflip29@hotmail.com>
2024-01-25 15:47:10 -0300

    3º --> funcionando

--- main.py
+++ main.py
@@ -1,13 +1,17 @@
+
 print('Código iniciado')
 print('')
 chosen_number = input('Digite o número teto do desafio, o número tem q ser inteiro: ')
 
-if chosen_number.isdigit():
-    chosen_number = int(chosen_number)
-    print('numérico')
-else:
-    print('O valor informado n é numérico ou n é inteiro. Execute o código novamente e digite um número inteiro')
-    quit()
+while True:
+    if chosen_number.isdigit():
+        chosen_number = int(chosen_number)
+        break
+    else:
+        print('O valor informado n é numérico ou n é inteiro.')
+        chosen_number = input('\nDigite o número teto do desafio, o número tem q ser inteiro: ')
+        continue
+
 
 from random import randint
 random_number = randint(0, chosen_number)
@@ -15,7 +19,7 @@ random_number = randint(0, chosen_number)
 counter = 0
 
 while True:
-    user_answer = input('Advinhe o número: ')
+    user_answer = input(f'\nAdvinhe o número de zero até {chosen_number}: ')
 
     if user_answer.isdigit():
         user_answer = int(user_answer)
